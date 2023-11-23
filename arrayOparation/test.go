5a20d2a538d47166f574531f17c68a559272904f
athunlal <athunlalp@gmail.com>
2023-11-23 16:28:09 +0530

    testing methods, insertion, bubble, selection(pending) and merge compeleted

--- arrayOparation/test.go
+++ arrayOparation/test.go
@@ -2,9 +2,44 @@ package main
 
 import "fmt"
 
-func insesrtSort(arr []int) {
-	n := len(arr)
-	for i := 1; i < n; i++ {
+func mergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	res1 := mergeSort(arr[len(arr)/2:])
+	res2 := mergeSort(arr[:len(arr)/2])
+
+	return merge(res1, res2)
+}
+
+func merge(arr1, arr2 []int) []int {
+	res := []int{}
+
+	i, j := 0, 0
+
+	for i < len(arr1) && j < len(arr2) {
+		if arr1[i] > arr2[j] {
+			res = append(res, arr2[j])
+			j++
+		} else {
+			res = append(res, arr1[i])
+			i++
+		}
+	}
+	for i < len(arr1) {
+		res = append(res, arr1[i])
+		i++
+	}
+
+	for j < len(arr2) {
+		res = append(res, arr2[j])
+		j++
+	}
+	return res
+}
+func inserSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
 		key := arr[i]
 		j := i - 1
 		for j >= 0 && arr[j] > key {
@@ -15,24 +50,18 @@ func insesrtSort(arr []int) {
 	}
 }
 func bubbleSort(arr []int) {
-
-	n := len(arr)
-	for i := 0; i < n-1; i++ {
-		for j := 0; j < n-1-i; j++ {
-			if arr[j] < arr[j+1] {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-1-i; j++ {
+			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
 		}
-
 	}
 }
 
 func main() {
-	arr := []int{8, 8, 9, 4, 5, 6, 1, 7, 2, 3, 3}
-
-	fmt.Println("Unsorted array:", arr)
-
+	arr := []int{9, 8, 5, 6, 2, 3, 1, 7, 4}
 	bubbleSort(arr)
 
-	fmt.Println("Sorted array:", arr)
+	fmt.Println(arr)
 }
