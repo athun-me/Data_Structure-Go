d10f9a54cea6d3f1ed76cefc7b470c6eb73d57a3
athunlal <athunlalp@gmail.com>
2023-12-28 19:57:35 +0530

    Tree

---
+++ TreeProblem/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Node struct {
+	val   int
+	left  *Node
+	right *Node
+}
+type TreeNode struct {
+	root *Node
+}
+
+func (t *TreeNode) insert(val int) {
+	newNode := &Node{val: val}
+	if t.root == nil {
+		t.root = newNode
+		return
+	}
+	t.root.insert(newNode)
+
+}
+func (n *Node) insert(newNode *Node) {
+	if n.val > newNode.val {
+		if n.left == nil {
+			n.left = newNode
+			return
+		} else {
+			n.left.insert(newNode)
+		}
+	} else {
+		if n.val < newNode.val {
+			if n.right == nil {
+				n.right = newNode
+				return
+			} else {
+				n.right.insert(newNode)
+			}
+		}
+	}
+}
+func inOrder(node *Node) {
+	if node != nil {
+		inOrder(node.left)
+		fmt.Println(node.val)
+		inOrder(node.right)
+	}
+}
+
+func depth(root *Node) int {
+	if root != nil {
+		left := depth(root.left)
+		right := depth(root.right)
+		if left < right {
+			return right + 1
+		} else {
+			return left + 1
+		}
+	}
+	return 0
+}
+func isSymmetri(root *Node) bool {
+	if root == nil {
+		return true
+	}
+	return isMirror(root.left, root.right)
+}
+
+func isMirror(left, right *Node) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+
+	return (left.val == right.val) &&
+		isMirror(left.left, right.right) &&
+		isMirror(left.right, right.left)
+}
+
+func isSame(root1, root2 *Node) bool {
+	if root1 == nil && root2 == nil {
+		return true
+	}
+	if root1 == nil || root2 == nil {
+		return false
+	}
+	return (root1.val == root2.val) &&
+		(isSame(root1.left, root2.left)) &&
+		(isSame(root1.right, root2.right))
+}
+
+func BFS(root *Node) {
+	if root == nil {
+		return
+	}
+	que := []*Node{root}
+
+	for len(que) > 0 {
+		for i := 0; i < len(que); i++ {
+			node := que[0]
+			que = que[1:]
+			fmt.Println(node.val)
+			if node.left != nil {
+				que = append(que, node.left)
+			}
+			if node.right != nil {
+				que = append(que, node.right)
+			}
+		}
+	}
+}
+
+func isValidBST(root *Node) bool {
+	var def func(root *Node, min, max int) bool
+
+	def = func(root *Node, min, max int) bool {
+		if root == nil {
+			return true
+		}
+		if root.val <= min || root.val >= max {
+			return false
+		}
+		return def(root.left, min, root.val) && def(root.right, root.val, max)
+	}
+
+	return def(root, math.MinInt, math.MaxInt)
+}
+
+func preOrder(root *Node) {
+	if root != nil {
+		preOrder(root.left)
+		preOrder(root.right)
+		fmt.Println(root.val)
+	}
+}
+
+func main() {
+
+	tree := &TreeNode{}
+	tree.insert(4)
+	tree.insert(2)
+	tree.insert(7)
+	tree.insert(1)
+	tree.insert(3)
+	tree.insert(9)
+	tree.insert(6)
+	preOrder(tree.root)
+}
